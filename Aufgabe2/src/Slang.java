7861ce54047c1e9f7a7345bd2582901bc6e485aa
Andreas <awiesing90@gmail.com>
2015-08-06 10:43:24 +0200

    Comments added.

--- Aufgabe2/src/Slang.java
+++ Aufgabe2/src/Slang.java
@@ -106,6 +106,16 @@ public class Slang {
     }
 
     /*fg: Atom functions */
+
+    /**
+     * Prints the content of the stack.
+     * Example: 1 2 3 .s
+     * --Stack:
+     * 3
+     * 2
+     * 1
+     * --bottom
+     */
     private static void printStack(){
         Stack<Value> tmpStack = new Stack<Value>();
         System.out.print("--Stack:\n");
@@ -119,30 +129,68 @@ public class Slang {
         }
     }
 
+    /**
+     * Removes the top element of the stack and prints it to the console without a new line.
+     */
     private static void f_prin(){
         System.out.print(myStack.pop().toS());
     }
+
+    /**
+     * Removes the top element of the stack and prints it to the console with a new line.
+     */
     private static void f_print(){
         System.out.println(myStack.pop().toS());
     }
+
+    /**
+     * Removes the top two elements of the stack and pushes element1 minus element2 to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_sub(){
         int t = myStack.pop().toI();
         myStack.push(new IntValue(myStack.pop().toI() - t));
     }
+
+    /**
+     * Removes the top two elements of the stack and pushes element1 plus element2 to the stack.
+     */
     private static void f_add(){
         myStack.push(new IntValue(myStack.pop().toI() + myStack.pop().toI()));
     }
+
+    /**
+     * Removes the top two elements of the stack and pushes element1 multiplicated by element2 to the stack.
+     */
     private static void f_mul(){
         myStack.push(new IntValue(myStack.pop().toI() * myStack.pop().toI()));
     }
+
+    /**
+     * Removes the top two elements of the stack and pushes element1 divided by element2 (Integer division) to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_div(){
         int t = myStack.pop().toI();
         myStack.push(new IntValue(myStack.pop().toI() / t));
     }
+
+    /**
+     * Removes the top two elements of the stack and pushes the rest of the division [element1 divided by element2] to
+     * the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_mod(){
         int t = myStack.pop().toI();
         myStack.push(new IntValue(myStack.pop().toI() % t));
     }
+
+    /**
+     * Removes the top element from the stack. If it is a String beginning with "http://" or "https://", the content
+     * of the website will be pushed to the stack as a String. Otherwise it will be assumed the element represents a
+     * file, which will be opened and its content will be pushed to the stack.
+     * @throws EvalError If the File could not be opened or could not be found.
+     */
     private static void f_read() throws EvalError {
         String s = myStack.pop().toS();
         Pattern p = Pattern.compile("\\Ahttps?://");
@@ -173,6 +221,12 @@ public class Slang {
             }
         }
     }
+
+    /**
+     * Overwrites the file [element1] with the string [element2] and removes those two elements from the stack.
+     * Element1 is the element which was added first and popped last.
+     * @throws EvalError If the file could not be opened or found.
+     */
     private static void f_write() throws EvalError{
         try {
             String s = myStack.pop().toS();
@@ -187,18 +241,41 @@ public class Slang {
             throw new EvalError(e.getMessage());
         }
     }
+
+    /**
+     * Prompts a JDialog which asks for Input by the User.
+     * The input of the user will be pushed to the stack as a String.
+     */
     private static void f_ask(){
         String inputValue = JOptionPane.showInputDialog(myStack.pop().toS());
         myStack.push(new StringValue(inputValue));
     }
+
+    /**
+     * Prompts a JDialog which asks for Input by the User.
+     * The input of the user will be pushed to the stack as an Integer Value.
+     */
     private static void f_askn(){
         int inputValue = Integer.parseInt(JOptionPane.showInputDialog(myStack.pop().toS()));
         myStack.push(new IntValue(inputValue));
     }
+
+    /**
+     * Removes the top two elements of the stack and appends element2 to element1. The result will be pushed to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_append(){
         String s = myStack.pop().toS();
         myStack.push(new StringValue(myStack.pop().toS() + s));
     }
+
+    /**
+     * Removes the top two elements of the stack and skips in element1 to the index, where element1 contains the String
+     * element2 for the first time. The rest of element1 beginning at this index will be pushed to the stack.
+     * If element1 does not contain the String element2, an empty string will be pushed to the stack.
+     * If the index is negative, bigger than the length of element1, an empty string will be pushed to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_skipto(){ //andreas
         String s = myStack.pop().toS();
         String s2 = myStack.pop().toS();
@@ -208,6 +285,14 @@ public class Slang {
             myStack.push(new StringValue(""));
         }
     }
+
+    /**
+     * Removes the top two elements of the stack and skips in element1 to the index [element2]. The rest of element1
+     * beginning at this index will be pushed to the stack. Element2 must be an integer value.
+     * If the index is negative, bigger than the length of element1 or not an integer value, an empty string will be
+     * pushed to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_skipn(){ //andreas
         Value s = myStack.pop();
         String s2 = myStack.pop().toS();
@@ -217,6 +302,14 @@ public class Slang {
             myStack.push(new StringValue(""));
         }
     }
+
+    /**
+     * Removes the top two elements of the stack and cuts element1 at the index [element2].
+     * The first part of element1 will be pushed to the stack (the index [element2] inclusive).
+     * If the index is negative or not a number, an empty string will be pushed to the stack.
+     * If the index is bigger than the length of element1, element1 will be pushed to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_trunc(){ //andreas
         Value s = myStack.pop();
         String s2 = myStack.pop().toS();
@@ -227,6 +320,13 @@ public class Slang {
             myStack.push(new StringValue(""));
         }
     }
+
+    /**
+     * Removes the top two elements of the stack. If element1 contains element2, element1 will be cut until the
+     * occurence of element2 and the first part of the result, element2 exclusive, will be pushed to the stack.
+     * If element1 does not contain element2, an empty string will be pushed to the stack.
+     * Element1 is the element which was added first and popped last.
+     */
     private static void f_copyto(){ //andreas
         String s = myStack.pop().toS();
         String s2 = myStack.pop().toS();
@@ -236,9 +336,17 @@ public class Slang {
             myStack.push(new StringValue(""));
         }
     }
+
+    /**
+     * Duplicates the top element of the stack. Therefore, peeks the stack and pushes back to the stack.
+     */
     private static void f_dup(){ //andreas
         myStack.push(myStack.peek());
     }
+
+    /**
+     * Removes the top element of the stack.
+     */
     private static void f_pop(){ //andreas
         myStack.pop();
     }
